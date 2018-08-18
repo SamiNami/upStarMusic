b0269c6e04f864c1b1d0fcf86af0f5fd23c59176
sami peralahti <sami.peralahti@vainu.io>
2018-08-17 21:24:58 -0700

    min and max age/years active works

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,15 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const findMin = Artist.findOne({})
+        .sort({ age: 1 })
+        .then(artist => artist.age);
+
+    const findMax = Artist.findOne({})
+        .sort({ age: -1 })
+        .then(artist => artist.age);
+
+    return Promise.all([findMin, findMax]).then(result => {
+        return { min: result[0], max: result[1] };
+    });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,15 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    const findMin = Artist.findOne({})
+        .sort({ age: 1 })
+        .then(artist => artist.yearsActive);
+
+    const findMax = Artist.findOne({})
+        .sort({ age: -1 })
+        .then(artist => artist.yearsActive);
+
+    return Promise.all([findMin, findMax]).then(result => {
+        return { min: result[0], max: result[1] };
+    });
 };
